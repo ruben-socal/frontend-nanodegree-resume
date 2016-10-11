2a9a3f7b8515f5b4943472a34fd4f3ad9475de4b
ruben-socal <ruben2045@gmail.com>
2016-10-11 10:38:57 -0700

    resumeBuilder property name corrections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,7 +11,7 @@ var bio = {
         "twitter": "@maximus_rm28",
         "location": "Los Angeles, CA"
     },
-    "welcomeMsg": "Welcome To My Resume",
+    "welcomeMessage": "Welcome To My Resume",
     "skills": [
         "HTML",
         " CSS",
@@ -20,7 +20,7 @@ var bio = {
         " Git",
         " Grunt"
     ],
-    "bioPic": "images/fry.jpg"
+    "biopic": "images/fry.jpg"
 };
 
 var education = {
@@ -40,7 +40,7 @@ var education = {
         "url": "http://www.elcamino.edu"
     }],
 
-    "onlineclasses": [{
+    "onlineCourses": [{
         "title": "Front-End Web Developer Nanodegree",
         "school": "Udacity",
         "dates": "2016",
@@ -68,7 +68,7 @@ var work = {
 };
 
 var projects = {
-    "project": [{
+    "projects": [{
         "title": "Personal Blog",
         "dates": "1-2015 to 3-2015",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
@@ -92,8 +92,8 @@ bio.display = function() {
     var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
     var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
     var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+    var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
 
     $("#header").prepend(formattedRole);
     $("#header").prepend(formattedName);
@@ -111,7 +111,7 @@ bio.display = function() {
     var i, len;
     for (i = 0, len = bio.skills.length; i < len; i++) {
         var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
-        $("#skillsH3").append(formattedSkills);
+        $("#skills").append(formattedSkills);
     }
 
     // Display footer contacts using bio data
@@ -165,15 +165,15 @@ education.display = function() {
         $(".education-entry:last").append(formattedSchoolMajors);
     }
 
-    // Display on-line classes array of objects with for loop
-    for (i = 0, len = education.onlineclasses.length; i < len; i++) {
+    // Display on-line Courses array of objects with for loop
+    for (i = 0, len = education.onlineCourses.length; i < len; i++) {
         $(".education-entry:last").append(HTMLonlineClasses);
-        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineclasses[i].title);
-        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineclasses[i].school);
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
         var formattedTitleSchool = formattedOnlineTitle + formattedOnlineSchool;
-        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineclasses[i].dates);
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
         // OR (|) operator is used to replace # and %data% with url data
-        var formattedOnlineUrl = HTMLonlineURL.replace(/%data%|#/g, education.onlineclasses[i].url);
+        var formattedOnlineUrl = HTMLonlineURL.replace(/%data%|#/g, education.onlineCourses[i].url);
 
         $(".education-entry:last").append(formattedTitleSchool);
         $(".education-entry:last").append(formattedOnlineDates);
@@ -185,19 +185,19 @@ education.display = function() {
 education.display();
 
 projects.display = function() {
-    //display project for loop
+    //display projects for loop
     var i, j, len, length;
-    for (i = 0, len = projects.project.length; i < len; i++) {
+    for (i = 0, len = projects.projects.length; i < len; i++) {
         $("#projects").append(HTMLprojectStart);
-        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title).replace("#", projects.project[i].url);
-        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
-        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.project[i].description);
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
         $(".project-entry:last").append(formattedProjectTitle);
         $(".project-entry:last").append(formattedProjectDates);
         $(".project-entry:last").append(formattedProjectDescription);
         // For loop needed if more than one image in images array
-        for (j = 0, length = projects.project[i].images.length; j < length; j++) {
-            var formattedProjectImages = HTMLprojectImage.replace("%data%", projects.project[i].images[j]);
+        for (j = 0, length = projects.projects[i].images.length; j < length; j++) {
+            var formattedProjectImages = HTMLprojectImage.replace("%data%", projects.projects[i].images[j]);
             $(".project-entry:last").append(formattedProjectImages);
         }
     }
